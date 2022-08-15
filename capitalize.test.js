9402b6749d20db3b2fb02dd3efa81279cbd893a9
Surafel Getachew <suragetch@gmail.com>
2022-08-15 21:28:38 +0300

    Add test case for capitalizd javascript file

---
+++ capitalize.test.js
@@ -0,0 +1,11 @@
+const capitalize = require("./capitalize");
+
+describe("Test capitalize function", () => {
+  test("Test Capitalization", () => {
+    expect(capitalize("programming")).toBe("Programming");
+  });
+
+  test('"microverse" test', () => {
+    expect(capitalize("microverse")).toBe("Microverse");
+  });
+});
