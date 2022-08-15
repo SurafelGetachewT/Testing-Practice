ffe084596c22d90e56265ee382d27f33dd238374
Surafel Getachew <suragetch@gmail.com>
2022-08-15 21:15:09 +0300

    Update String length javascript file and test

--- stringLLength.test.js
+++ stringLLength.test.js
@@ -1,5 +1,17 @@
-const reverseString = require("./reverseString");
+const stringLength = require("./stringLength");
+describe("Testing for length of String", () => {
+  test('Length of "Sura"', () => {
+    expect(stringLength("Sura")).toBe(4);
+  });
+  test("Empty String Length", () => {
+    expect(stringLength("")).toThrow(
+      "The string should be at least 1 character long and not more than 10"
+    );
+  });
 
-test("Reversed string Should be", () => {
-  expect(reverseString("Surafel")).toBe("lefaruS");
+  test('"Surafel Getchew" String Length', () => {
+    expect(stringLength("Surafel Getchew")).toThrow(
+      "The string should be at least 1 character long and not more than 10"
+    );
+  });
 });
